a812e23aeb0aa59e25e3c363b674887c3e7650d9
vaishnavi sonu <vaishnavisonu25@gmail.com>
2024-05-28 19:30:07 +0530

    forkify

---
+++ src/js/config.js
@@ -0,0 +1,6 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+// export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'f62e2d0c-4557-4b3a-908a-503ce55ad229';
+export const MODAL_CLOSE_SEC = 2.5;
